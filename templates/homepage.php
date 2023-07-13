c50043019ce0ee5aab6a8b4b4fafa89bfd2561e4
j-lucea <jonathan.lucea@gmail.com>
2023-07-13 20:35:42 +0200

    Clean Blog Bootstrap theme added V1

--- templates/homepage.php
+++ templates/homepage.php
@@ -1,8 +1,20 @@
 <?php $title = "Le blog de l'AVBN"; ?>
 
 <?php ob_start(); ?>
-<h1>Le super blog de l'AVBN !</h1>
 <p>Derniers billets du blog :</p>
+<!-- Page Header-->
+<header class="masthead" style="background-image: url('assets/img/home-bg.jpg')">
+    <div class="container position-relative px-4 px-lg-5">
+        <div class="row gx-4 gx-lg-5 justify-content-center">
+            <div class="col-md-10 col-lg-8 col-xl-7">
+                <div class="site-heading">
+                    <h1>My First Blog</h1>
+                    <span class="subheading">An introduction to my professional life</span>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
 
 <?php
 foreach ($posts as $post) {
