f99aba9f2147ad07df8aa44d1aabb40372457297
j-lucea <jonathan.lucea@gmail.com>
2023-07-14 18:24:45 +0200

    Homepage Theme integration

--- templates/layout.php
+++ templates/layout.php
@@ -6,30 +6,30 @@
     <meta name="description" content="" />
     <meta name="author" content="" />
     <title><?= $title ?></title>
-    <link rel="icon" type="image/x-icon" href="startbootstrap-clean-blog/assets/favicon.ico" />
+    <link rel="icon" type="image/x-icon" href="templates/startbootstrap-clean-blog/assets/favicon.ico" />
     <!-- Font Awesome icons (free version)-->
     <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
     <!-- Google fonts-->
     <link href="https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic" rel="stylesheet" type="text/css" />
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800" rel="stylesheet" type="text/css" />
     <!-- Core theme CSS (includes Bootstrap)-->
-    <link href="startbootstrap-clean-blog/css/styles.css" rel="stylesheet" />
+    <link href="templates/startbootstrap-clean-blog/css/styles.css" rel="stylesheet" />
 </head>
 <body>
 <!-- Navigation-->
 <nav class="navbar navbar-expand-lg navbar-light" id="mainNav">
     <div class="container px-4 px-lg-5">
-        <a class="navbar-brand" href="startbootstrap-clean-blog/index.html">Start Bootstrap</a>
+        <a class="navbar-brand" href="templates/startbootstrap-clean-blog/index.html">Start Bootstrap</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
             Menu
             <i class="fas fa-bars"></i>
         </button>
         <div class="collapse navbar-collapse" id="navbarResponsive">
             <ul class="navbar-nav ms-auto py-4 py-lg-0">
-                <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="startbootstrap-clean-blog/index.html">Home</a></li>
-                <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="startbootstrap-clean-blog/about.html">About</a></li>
-                <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="startbootstrap-clean-blog/post.html">Sample Post</a></li>
-                <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="startbootstrap-clean-blog/contact.html">Contact</a></li>
+                <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="templates/startbootstrap-clean-blog/index.html">Home</a></li>
+                <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="templates/startbootstrap-clean-blog/about.html">About</a></li>
+                <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="templates/startbootstrap-clean-blog/post.html">Sample Post</a></li>
+                <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="templates/startbootstrap-clean-blog/contact.html">Contact</a></li>
             </ul>
         </div>
     </div>
@@ -79,7 +79,7 @@
 <!-- Bootstrap core JS-->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
 <!-- Core theme JS-->
-<script src="startbootstrap-clean-blog/js/scripts.js"></script>
+<script src="templates/startbootstrap-clean-blog/js/scripts.js"></script>
 </body>
 </html>
 
