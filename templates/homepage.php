4b2b62205f44861f4947a0a58a4a3d2938e1e7a6
j-lucea <jonathan.lucea@gmail.com>
2023-07-14 18:52:16 +0200

    Homepage Theme integration V2

--- templates/homepage.php
+++ templates/homepage.php
@@ -1,9 +1,9 @@
 <?php $title = "Le blog de l'AVBN"; ?>
 
 <?php ob_start(); ?>
-<p>Derniers billets du blog :</p>
+<!-- <p>Derniers billets du blog :</p> -->
 <!-- Page Header-->
-<header class="masthead" style="background-image: url('startbootstrap-clean-blog/assets/img/home-bg.jpg')">
+<header class="masthead" style="background-image: url('templates/startbootstrap-clean-blog/assets/img/home-bg.jpg')">
     <div class="container position-relative px-4 px-lg-5">
         <div class="row gx-4 gx-lg-5 justify-content-center">
             <div class="col-md-10 col-lg-8 col-xl-7">
