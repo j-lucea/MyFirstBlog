485a5ed6fc00d050277cec15df3812932050a4cb
j-lucea <jonathan.lucea@gmail.com>
2023-07-14 18:24:45 +0200

    Homepage Theme integration

--- templates/homepage.php
+++ templates/homepage.php
@@ -3,7 +3,7 @@
 <?php ob_start(); ?>
 <p>Derniers billets du blog :</p>
 <!-- Page Header-->
-<header class="masthead" style="background-image: url('assets/img/home-bg.jpg')">
+<header class="masthead" style="background-image: url('startbootstrap-clean-blog/assets/img/home-bg.jpg')">
     <div class="container position-relative px-4 px-lg-5">
         <div class="row gx-4 gx-lg-5 justify-content-center">
             <div class="col-md-10 col-lg-8 col-xl-7">
@@ -15,22 +15,29 @@
         </div>
     </div>
 </header>
-
+<!-- Main Content-->
+<div class="container px-4 px-lg-5">
+    <div class="row gx-4 gx-lg-5 justify-content-center">
+        <div class="col-md-10 col-lg-8 col-xl-7">
 <?php
 foreach ($posts as $post) {
-?>
-    <div class="news">
-        <h3>
-            <?= htmlspecialchars($post->title); ?>
+    ?>
+    <!-- Post preview-->
+    <div class="post-preview">
+        <a href="index.php?action=post&id=<?= urlencode($post->identifier) ?>">
+            <h2 class="post-title"><?= htmlspecialchars($post->title); ?></h2>
+            <h3 class="post-subtitle"><?= nl2br(htmlspecialchars($post->content)); ?></h3>
+        </a>
+        <p class="post-meta">
+            <!-- Part in order to display author later
+            Par
+            <a href="#!"><em>le <?= $post->author; ?></em></a> -->
             <em>le <?= $post->frenchCreationDate; ?></em>
-        </h3>
-        <p>
-            <?= nl2br(htmlspecialchars($post->content)); ?>
-            <br />
-            <em><a href="index.php?action=post&id=<?= urlencode($post->identifier) ?>">Commentaires</a></em>
         </p>
     </div>
-<?php
+    <!-- Divider-->
+    <hr class="my-4" />
+    <?php
 }
 ?>
 <?php $content = ob_get_clean(); ?>
