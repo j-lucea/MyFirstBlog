bff59871d6f7e22db9531784d00e9cc4904e3851
j-lucea <jonathan.lucea@gmail.com>
2023-07-22 15:34:01 +0200

    Repository folder created in the src one with CommentRepository and PostRepository files

---
+++ src/repository/postRepository.php
@@ -0,0 +1,45 @@
+<?php
+
+use Application\Lib\Database\DatabaseConnection;
+use Application\Model\Post\Post;
+
+class PostRepository
+{
+    public DatabaseConnection $connection;
+
+    public function getPost(string $identifier): Post
+    {
+        $statement = $this->connection->getConnection()->prepare(
+            "SELECT id, title, content, DATE_FORMAT(creation_date, '%d/%m/%Y à %Hh%imin%ss') AS french_creation_date FROM p5_post WHERE id = ?"
+        );
+        $statement->execute([$identifier]);
+
+        $row = $statement->fetch();
+        $post = new Post();
+        $post->title = $row['title'];
+        $post->frenchCreationDate = $row['french_creation_date'];
+        $post->content = $row['content'];
+        $post->identifier = $row['id'];
+
+        return $post;
+    }
+
+    public function getPosts(): array
+    {
+        $statement = $this->connection->getConnection()->query(
+            "SELECT id, title, content, DATE_FORMAT(creation_date, '%d/%m/%Y à %Hh%imin%ss') AS french_creation_date FROM p5_post ORDER BY creation_date DESC LIMIT 0, 5"
+        );
+        $posts = [];
+        while (($row = $statement->fetch())) {
+            $post = new Post();
+            $post->title = $row['title'];
+            $post->frenchCreationDate = $row['french_creation_date'];
+            $post->content = $row['content'];
+            $post->identifier = $row['id'];
+
+            $posts[] = $post;
+        }
+
+        return $posts;
+    }
+}
