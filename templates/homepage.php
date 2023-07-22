235faf6ce9721f8e656847efb60e46dd18803df1
j-lucea <jonathan.lucea@gmail.com>
2023-07-22 12:55:11 +0200

    "startbootstrap-clean-blog" folder renamed by "public" and placed in the "src" folder

--- templates/homepage.php
+++ templates/homepage.php
@@ -3,7 +3,7 @@
 <?php ob_start(); ?>
 <!-- <p>Derniers billets du blog :</p> -->
 <!-- Page Header-->
-<header class="masthead" style="background-image: url('templates/startbootstrap-clean-blog/assets/img/home-bg.jpg')">
+<header class="masthead" style="background-image: url('src/public/assets/img/home-bg.jpg')">
     <div class="container position-relative px-4 px-lg-5">
         <div class="row gx-4 gx-lg-5 justify-content-center">
             <div class="col-md-10 col-lg-8 col-xl-7">
