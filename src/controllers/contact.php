efe25824466fb1a9c337a9f6630787d30e8012da
j-lucea <jonathan.lucea@gmail.com>
2023-08-20 17:10:56 +0200

    PHP Mailer folder imported

--- src/controllers/contact.php
+++ src/controllers/contact.php
@@ -1,12 +1,32 @@
 <?php
 
-
 namespace Application\Controllers\Contact;
+
+use Application\PHPMailer\PHPMailer\PHPMailer;
+use Application\PHPMailer\PHPMailer\Exception;
+
+require 'src/phpmailer/src/Exception.php';
+require 'src/phpmailer/src/PHPMailer.php';
+require 'src/phpmailer/src/SMTP.php';
 
 class Contact
 {
     public function execute()
     {
+        if (isset($_POST['name']) && isset($_POST['email'])
+            && isset($_POST['phone']) && isset($_POST['message'])) {
+            $mail = new PHPMailer();
+            $mail->IsSMTP();
+            $mail->Host = 'dns106.ovh.net';               //Adresse IP ou DNS du serveur SMTP
+            $mail->Port = 465;                          //Port TCP du serveur SMTP
+            $mail->SMTPAuth = 1;                        //Utiliser l'identification
+
+            if($mail->SMTPAuth){
+                $mail->SMTPSecure = 'ssl';               //Protocole de sécurisation des échanges avec le SMTP
+                $mail->Username   =  'contact@jlucea.com';   //Adresse email à utiliser
+                $mail->Password   =  'Nathan2018';         //Mot de passe de l'adresse email à utiliser
+            }
+        }
 
         require('templates/contact.php');
     }
--- templates/contact.php
+++ templates/contact.php
@@ -23,55 +23,32 @@
                     vers vous dès que possible !
                 </p>
                 <div class="my-5">
-                    <!-- * * * * * * * * * * * * * * *-->
-                    <!-- * * SB Forms Contact Form * *-->
-                    <!-- * * * * * * * * * * * * * * *-->
-                    <!-- This form is pre-integrated with SB Forms.-->
-                    <!-- To make this form functional, sign up at-->
-                    <!-- https://startbootstrap.com/solution/contact-forms-->
-                    <!-- to get an API token!-->
-                    <form id="contactForm" data-sb-form-api-token="API_TOKEN">
+                    <!-- Contact Form -->
+                    <form id="contactForm" action="index.php?action=contact" method="post">
                         <div class="form-floating">
                             <input class="form-control" id="name" type="text"
                                    placeholder="Entrez votre nom..."
-                                   data-sb-validations="required" />
+                                   required />
                             <label for="name">Nom</label>
-                            <div class="invalid-feedback"
-                                 data-sb-feedback="name:required">Un nom est nécessaire.
-                            </div>
                         </div>
                         <div class="form-floating">
                             <input class="form-control" id="email" type="email"
                                    placeholder="Entrez votre adresse mail..."
-                                   data-sb-validations="required,email" />
+                                  required />
                             <label for="email">Addresse mail</label>
-                            <div class="invalid-feedback" data-sb-feedback="email:required">
-                                Une adresse mail est nécessaire.
-                            </div>
-                            <div class="invalid-feedback" data-sb-feedback="email:email">
-                                Adresse mail invalide.
-                            </div>
                         </div>
                         <div class="form-floating">
                             <input class="form-control" id="phone" type="tel"
                                    placeholder="Entrez votre numéro de téléphone..."
-                                   data-sb-validations="required" />
+                                   required />
                             <label for="phone">Téléphone</label>
-                            <div class="invalid-feedback"
-                                 data-sb-feedback="phone:required">
-                                Un numéro de téléphone est nécessaire.
-                            </div>
                         </div>
                         <div class="form-floating">
                             <textarea class="form-control" id="message"
                                       placeholder="Entrez votre message ici..."
-                                      style="height: 12rem" data-sb-validations="required">
+                                      style="height: 12rem" required>
                             </textarea>
                             <label for="message">Message</label>
-                            <div class="invalid-feedback"
-                                 data-sb-feedback="message:required">
-                                Un message est nécessaire.
-                            </div>
                         </div>
                         <br />
                         <!-- Submit success message-->
@@ -81,11 +58,6 @@
                         <div class="d-none" id="submitSuccessMessage">
                             <div class="text-center mb-3">
                                 <div class="fw-bolder">Form submission successful!</div>
-                                To activate this form, sign up at
-                                <br />
-                                <a href="https://startbootstrap.com/solution/contact-forms">
-                                    https://startbootstrap.com/solution/contact-forms
-                                </a>
                             </div>
                         </div>
                         <!-- Submit error message-->
@@ -98,7 +70,7 @@
                             </div>
                         </div>
                         <!-- Submit Button-->
-                        <button class="btn btn-primary text-uppercase disabled"
+                        <button class="btn btn-primary text-uppercase"
                                 id="submitButton" type="submit">Envoyer
                         </button>
                     </form>
